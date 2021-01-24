99e2cbcc9f033b7eeaa863eabddaab4ae80dcf0b
Juan Sifontez <juansfontz@gmail.com>
2021-01-24 14:40:15 -0400

    feat: Add country api call

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import QuizList from './QuizList'
 import './Modal.css'
+import randomCountry from '../utils/randomCountry'
 
 const Modal = (props) => {
   const [gameMode, setGameMode ] = useState("Select game mode")
   const [isLoading, setIsLoading] = useState(false)
+  const [query, setQuery] = useState(false)
+  const [data, setData] = useState({name: '', capital: ''})
+
+  useEffect(() => {
+    if (query === false) return
+    const fetchData = async () => {
+      const countryCode = randomCountry()
+      const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${countryCode}`)
+      const country = await res.json()
+
+      setData({name: country.name, capital: country.capital})
+      setQuery(false)
+    }
+
+    fetchData()
+  }, [query])
 
   const changeGameMode = (mode) => {
     setIsLoading(true)
     setGameMode(mode)
+    setQuery(true)
   }
 
   let quiz;
