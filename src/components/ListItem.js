ebfd5dcd26c9d821aa6e3c361c79ee18d4e78322
Juan <juansfontz@gmail.com>
2021-02-08 16:58:12 -0400

    style: add checked and close icons

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -29,6 +29,10 @@ const ListItem = ({text, changeGameMode, gameMode, countryChoice}) => {
         onClick={handleChoice}
       >
         {text}
+        <span className="option">
+          <img className="option__img"  src="check_circle_outline-white-24dp.svg" alt="rounded checked icon" />
+          <img className="option__img"  src="close-black-24dp.svg" alt="X icon" />
+        </span>
       </li>
     )
   }
