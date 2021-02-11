89e999f17ad12ef6d588187474cf04d65d662f18
Juan <juansfontz@gmail.com>
2021-02-10 21:24:32 -0400

    style(app): add background to the game

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Modal from './components/Modal'
 
 function App() {
   return (
-    <div className="app__container">
+    <div className="app__container" style={{backgroundImage: 'url("./background.png")'}}>
       <main className="App">
         <h1>COUNTRY QUIZ</h1>
         <Modal />
