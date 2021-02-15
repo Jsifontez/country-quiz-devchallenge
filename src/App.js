2ce1d606618a4a0a4391c060fedb20bf22f495ac
Juan <juansfontz@gmail.com>
2021-02-14 20:52:44 -0400

    feat: Add footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <h1>COUNTRY QUIZ</h1>
         <Modal />
       </main>
+      <footer className="footer">
+        <p>
+          Made with ❤️ by <a href="https://twitter.com/jsifontez_" target="_blank" rel="noreferrer">@jsifontez</a>
+        </p>
+        <p>
+          Commits are welcome on <a href="https://twitter.com/jsifontez_" target="_blank" rel="noreferrer">GitHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
